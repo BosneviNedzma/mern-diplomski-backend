c1f7d64f88428c80dd9760d79e95eee6c9d03031
Nedzma Brkic <brkicnedzma@gmail.com>
2024-09-05 11:07:11 +0200

    Update

--- src/firebaseAdminConfig.ts
+++ src/firebaseAdminConfig.ts
@@ -1,8 +1,16 @@
 // src/firebaseAdminConfig.ts
 import * as admin from 'firebase-admin';
 import * as path from 'path';
+import * as fs from 'fs';
 
-const serviceAccountPath = path.resolve(__dirname, './config/firebase/serviceAccountKey.json');
+// Koristi path.join za izgradnju putanje
+const serviceAccountPath = path.join(__dirname, '../config/firebase/serviceAccountKey.json');
+
+// Proveri da li fajl postoji na izgradjenoj putanji
+if (!fs.existsSync(serviceAccountPath)) {
+  throw new Error(`Service account file not found at ${serviceAccountPath}`);
+}
+
 const serviceAccount = require(serviceAccountPath);
 
 admin.initializeApp({
